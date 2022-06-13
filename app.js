2f33e5c5c8ace018c19e6d6d2f60bbb0f031d2a2
OkoyeCharles <okoyecharles509@gmail.com>
2022-06-13 20:19:23 +0100

    Add Foor loop to js File

--- app.js
+++ app.js
@@ -2,25 +2,15 @@ const navLinks = document.querySelector('.nav-links');
 const hamburger = document.getElementById('open-menu');
 const closeMenu = document.getElementById('close-menu');
 
-const navLink1 = document.getElementById('nav-link1');
-const navLink2 = document.getElementById('nav-link2');
-const navLink3 = document.getElementById('nav-link3');
+const navLink = Array.from(document.querySelectorAll('.nav-link'));
 
 const wrap = document.querySelector('.wrapper');
 
-// listen for individual clicks
-navLink1.addEventListener('click', () => {
+// this will loop over each link when clicked
+navLink.forEach((link) => link.addEventListener('click', () => {
   navLinks.classList.toggle('active');
   wrap.classList.toggle('w-active');
-});
-navLink2.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-  wrap.classList.toggle('w-active');
-});
-navLink3.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-  wrap.classList.toggle('w-active');
-});
+}));
 
 // listen for hamburger click
 hamburger.addEventListener('click', () => {
