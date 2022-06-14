b749656b987cacbbd507a8996a93c9f91b337b22
OkoyeCharles <okoyecharles509@gmail.com>
2022-06-14 23:14:12 +0100

    Add Linter Checks

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const navLinks = document.querySelector('.nav-links');
 const hamburger = document.getElementById('open-menu');
 const closeMenu = document.getElementById('close-menu');
 const workSection = document.getElementById('work-section');
-const cardPopup = document.querySelector('.popup')
-const body = document.querySelector('.body')
+const cardPopup = document.querySelector('.popup');
+const body = document.querySelector('.body');
 
 const navLink = Array.from(document.querySelectorAll('.nav-link'));
 
@@ -105,7 +105,7 @@ const createCard = (arr, i) => {
   cardList.className = 'card-list';
   const cardButton = document.createElement('button');
   cardButton.classList.add('card-button', 'button');
-  cardButton.id = `${i}`
+  cardButton.id = `${i}`;
 
   workSection.appendChild(cardContainer);
   cardContainer.append(cardImage, cardContent);
@@ -130,12 +130,89 @@ const createCard = (arr, i) => {
 const cardCount = Array.from(Array(cardArray.length).keys());
 cardCount.forEach((x) => createCard(cardArray, x));
 
+// This Function deletes the Popup
+const deletePopup = () => {
+  while (cardPopup.firstChild) {
+    cardPopup.removeChild(cardPopup.firstChild);
+  }
+};
 
+// This Function creates the Popup
+const createPopup = (arr, i) => {
+  // Set up Popup Layout
+  const headerDiv = document.createElement('div');
+  headerDiv.className = 'popup-header';
+  const headerContent = document.createElement('div');
+  headerContent.className = 'popup-h-content';
+  const popupHeading = document.createElement('h1');
+  popupHeading.className = 'popup-content-heading';
+  popupHeading.innerHTML = arr[i].name;
+  const popupTime = document.createElement('div');
+  popupTime.className = 'popup-content-time';
+  const popupClose = document.createElement('img');
+  popupClose.src = 'images/dark-close.png';
+  const popupImage = document.createElement('img');
+  popupImage.src = arr[i].image;
+  const popupContent = document.createElement('div');
+  popupContent.className = 'popup-content';
+  const popupText = document.createElement('div');
+  popupText.className = 'popup-text';
+  popupText.innerHTML = arr[i].longDescription;
+  const popupLangink = document.createElement('div');
+  popupLangink.className = 'popup-lang-link';
+  const popupLanguages = document.createElement('div');
+  popupLanguages.className = 'popup-lang';
+  const popupLinks = document.createElement('div');
+  popupLinks.className = 'popup-links';
+  const popupButton1 = document.createElement('button');
+  popupButton1.innerHTML = 'See Live';
+  const popupButton2 = document.createElement('button');
+  popupButton2.innerHTML = 'See Source';
+  const popupbtnimg1 = document.createElement('img');
+  popupbtnimg1.src = 'images/seelive.png';
+  const popupbtnimg2 = document.createElement('img');
+  popupbtnimg2.src = 'images/githubinbutton.png';
 
+  // Append Items to required parents
+  cardPopup.append(headerDiv, popupImage, popupContent);
+  headerDiv.append(headerContent, popupTime);
+  headerContent.append(popupHeading, popupClose);
+  popupContent.append(popupText, popupLangink);
+  popupLangink.append(popupLanguages, popupLinks);
+  popupTime.append(document.createElement('p'), document.createElement('img'), document.createElement('p'), document.createElement('img'), document.createElement('p'));
+  popupTime.children[0].innerHTML = arr[i].time.text1;
+  popupTime.children[1].src = arr[i].time.img;
+  popupTime.children[2].innerHTML = arr[i].time.text2;
+  popupTime.children[3].src = arr[i].time.img;
+  popupTime.children[4].innerHTML = arr[i].time.text3;
+  popupButton1.appendChild(popupbtnimg1);
+  popupButton2.appendChild(popupbtnimg2);
 
+  // Iterate through the Languages in CardArray and Append each Language
+  const iterators = Array.from(Array(arr[i].languages.length).keys());
+  iterators.forEach((x) => {
+    popupLanguages.appendChild(document.createElement('li'));
+    const line = popupLanguages.children[x];
+    line.innerHTML = arr[i].languages[x];
+  });
+  popupLinks.append(popupButton1, popupButton2);
 
+  // Add Close functionality to Close Button (This is where the delete popup function is used)
+  popupClose.addEventListener('click', () => {
+    cardPopup.classList.toggle('active');
+    wrap.classList.toggle('blurred');
+    body.classList.toggle('unscroll');
+    deletePopup();
+  });
+};
 
-
-
-
-
+// This button triggers Popup creation based on cards they're clicked from
+const buttons = Array.from(document.querySelectorAll('.card-content .button'));
+buttons.forEach((x) => x.addEventListener('click', () => {
+  if (!cardPopup.classList.contains('active')) {
+    cardPopup.classList.toggle('active');
+    createPopup(cardArray, x.id);
+    wrap.classList.toggle('blurred');
+    body.classList.toggle('unscroll');
+  }
+}));
